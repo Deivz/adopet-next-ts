293ea884115150d580d5db6616b6550d2d0b5ef1
Deivz <carlosd_oliveira@hotmail.com>
2023-04-29 00:27:19 -0300

    Form validations TBI. Home now is consuming from API, tests TBI

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,39 @@
 import Card from '@/components/Card';
 import styles from '@/styles/Home.module.css';
-import { useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 
-export default function Home() {
+type HomeProps = {
+    petsJson: SetStateAction<never[]> ;
+}
+
+type Pet = {
+    id: string;
+    nome: string;
+    idade: string;
+    porte: string;
+    perfil: string;
+    cidade: string;
+    estado: string;
+    foto: any;
+}
+
+export async function getStaticProps(){
+    const data = await fetch('http://127.0.0.1:8000/api/pets');
+    // const data = await fetch('https://jsonplaceholder.typicode.com/todos');
+
+    const petsJson = await data.json();
+    
+    return {
+        props: {
+            petsJson
+        },
+      };
+}
+
+export default function Home({ petsJson }: HomeProps) {
     const [pets, setPets] = useState([]);
     useEffect(() => {
-      //   api.get('/pets').then((response) => {
-      //       setPets(response.data);
-      //   })
+        setPets(petsJson);
     }, []);
 
     return (
@@ -18,15 +44,15 @@ export default function Home() {
                 <h2 className={styles.title}>Veja os amigos disponíveis para adoção!</h2>
             </div>
             <div className={styles.cards}>
-                {Object.values(pets).map((pet) => {
+                {Object.values(pets).map((pet: Pet) => {
                     return <Card
-                        key='{pet.id}'
-                        image='{pet.image}'
-                        name='{pet.name}'
-                        age='{pet.age}'
-                        size='{pet.size}'
-                        temperament='{pet.temperament}'
-                        local='{pet.local}'
+                        key={pet.id}
+                        image={pet.foto}
+                        name={pet.nome}
+                        age={pet.idade}
+                        size={pet.porte}
+                        temperament={pet.perfil}
+                        local={`${pet.cidade} (${pet.estado})`}
                     />
                 })}
             </div>
